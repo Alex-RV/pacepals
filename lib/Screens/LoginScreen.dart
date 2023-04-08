be122dde5a25a6b477885d04e2bf49d27fb01c78
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-04-08 14:50:52 -0700

    added login page

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -7,118 +9,103 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-TextEditingController nameController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
- 
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'TutorialKart',
-                  style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 30),
-                )),
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Sign in',
-                  style: TextStyle(fontSize: 20),
-                )),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'User Name',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                //forgot password screen
-              },
-              child: const Text('Forgot Password',),
-            ),
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  child: const Text('Login'),
-                  onPressed: () {
-                    print(nameController.text);
-                    print(passwordController.text);
-                  },
-                )
-            ),
-            Row(
-              children: <Widget>[
-                const Text('Does not have account?'),
-                TextButton(
-                  child: const Text(
-                    'Sign in',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  onPressed: () {
-                    //signup screen
-                  },
-                )
-              ],
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-          ],
-        ));
+    return Stack(children: <Widget>[
+      Image.asset(
+        "assets/pacepalsBackground.png",
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        fit: BoxFit.cover,
+      ),
+      ClipRRect(
+        // Clip it cleanly.
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+          child: Container(
+              color: Color.fromARGB(255, 52, 52, 52).withOpacity(0.4),
+              alignment: Alignment.center,
+              child: Scaffold(
+                  backgroundColor: Colors.transparent,
+                  body: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                          alignment: Alignment.center,
+                          padding: const EdgeInsets.all(10),
+                          child: const Text(
+                            'Sign in',
+                            style: TextStyle(fontSize: 24, color: Colors.white),
+                          )),
+                    // User Name Text Field
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        child: TextField(
+                          controller: nameController,
+                          style: const TextStyle(
+                            color: Colors.white, // text color
+                          ),
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: const Color.fromARGB(136, 75, 75, 75),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              borderSide: const BorderSide(color: Colors.white),
+                            ),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: const BorderSide(
+                                    color: Color.fromARGB(227, 2, 0, 0))),
+                            labelText: 'User Name',
+                            labelStyle: const TextStyle(
+                              color: Colors.white, // label color when focused
+                            ),
+                          ),
+                        ),
+                      ),
+                    // Password Text Field
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                        child: TextField(
+                          obscureText: true,
+                          controller: passwordController,
+                          style: const TextStyle(
+                            color: Colors.white, // text color
+                          ),
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: const Color.fromARGB(136, 75, 75, 75),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              borderSide: const BorderSide(color: Colors.white),
+                            ),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide: const BorderSide(
+                                    color: Color.fromARGB(227, 2, 0, 0))),
+                            labelText: 'Password',
+                            labelStyle: const TextStyle(
+                              color: Colors.white, // label color when focused
+                            ),
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          //forgot password screen
+                        },
+                        child: const Text(
+                          'Forgot Password',
+                        ),
+                      ),
+                    ],
+                  ))),
+        ),
+      ),
+    ]);
   }
-
-  // void _signInWithEmailAndPassword() async {
-  //   try {
-  //     final User user = (await _auth.signInWithEmailAndPassword(
-  //       email: _email,
-  //       password: _password,
-  //     ))
-  //         .user;
-  //     if (user != null) {
-  //       Navigator.of(context).pushReplacementNamed("/home");
-  //     }
-  //   } catch (e) {
-  //     setState(() {
-  //       _isLoading = false;
-  //     });
-  //     showDialog(
-  //       context: context,
-  //       builder: (BuildContext context) {
-  //         return AlertDialog(
-  //           title: Text("Error"),
-  //           content: Text(e.message),
-  //           actions: [
-  //             FlatButton(
-  //               onPressed: () => Navigator.of(context).pop(),
-  //               child: Text("OK"),
-  //             ),
-  //           ],
-  //         );
-  //       },
-  //     );
-  //   }
-  // }
 }
