7a55e84b087fd0f171d6199e2ee51d39aa2a0b0b
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2023-04-08 12:16:06 -0700

    Navbar

---
+++ lib/Widgets/Navbar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Title"),
+      ),
+    );
+  }
+}
