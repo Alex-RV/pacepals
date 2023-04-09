6072bce9cef3d906a0be3b491375a35442ae93e4
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-04-08 23:37:58 -0700

    added qr code

--- lib/Screens/SettingScreen.dart
+++ lib/Screens/SettingScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_session_manager/flutter_session_manager.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 import 'SignInScreen.dart';
 
@@ -11,25 +12,109 @@ class SettingScreen extends StatefulWidget {
 }
 
 class _SettingScreenState extends State<SettingScreen> {
+  Future<String> getFullname() async {
+    return await SessionManager().get("fullname");
+  }
+
+  Future<String> getEmail() async {
+    return await SessionManager().get("email");
+  }
+
+  Future<String> getUid() async {
+    return await SessionManager().get("uid");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [Container(child: ElevatedButton(
-  onPressed: () async {
-    await SessionManager().remove("sid");
-    Navigator.of(context).pushAndRemoveUntil(
-      MaterialPageRoute(builder: (context) => SignInScreen()),
-      (Route<dynamic> route) => false,
-    );
-  },
-  style: ElevatedButton.styleFrom(
-    backgroundColor: Colors.red,
-    textStyle: TextStyle(fontSize: 20),
-    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-  ),
-  child: Text("Sign Out"),
-)
-)]),
-    );
+        appBar: AppBar(title: Text("Settings")),
+        body: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Text("Name"),
+                      //User name
+                      FutureBuilder<String>(
+                        future: getFullname(),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<String> snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const CircularProgressIndicator();
+                          } else {
+                            return Text(snapshot.data ?? '');
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Text("Email"),
+                      //User email
+                      FutureBuilder<String>(
+                        future: getEmail(),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<String> snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const CircularProgressIndicator();
+                          } else {
+                            return Text(snapshot.data ?? '');
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Text("QR Code"),
+                      //User email
+                      FutureBuilder<String>(
+                        future: getUid(),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<String> snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const CircularProgressIndicator();
+                          } else {
+                            return QrImage(
+                              data: snapshot.data ?? '',
+                              version: QrVersions.auto,
+                              size: 200.0,
+                            );
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                    child: ElevatedButton(
+                  onPressed: () async {
+                    await SessionManager().destroy();
+                    Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(builder: (context) => SignInScreen()),
+                      (Route<dynamic> route) => false,
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                    textStyle: TextStyle(fontSize: 20),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  ),
+                  child: Text("Sign Out"),
+                ))
+              ]),
+        ));
   }
 }
