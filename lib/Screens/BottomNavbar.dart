32e5a1e58d9175206cca1113f5d4eccd5850f58d
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2023-04-08 20:54:21 -0700

    navbar

--- lib/Screens/BottomNavbar.dart
+++ lib/Screens/BottomNavbar.dart
@@ -1,87 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'HomeScreen.dart';
 
 void main() => runApp(const BottomNavBar());
 
-class BottomNavBar extends StatelessWidget {
-  const BottomNavBar({Key? key});
-
-  static const String _title = 'Flutter Code Sample';
-
+class BottomNavBar extends StatefulWidget{
+  const BottomNavBar({Key? key}) : super (key: key);
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: _title,
-      home: BottomNavbarState(),
-    );
-  }
+  State<BottomNavBar> createState() => _BottomNavBar();
 }
 
-class BottomNavbarState extends StatefulWidget {
-  const BottomNavbarState({Key? key});
 
-  @override
-  State<BottomNavbarState> createState() => _BottomNavbar();
-}
-
-class _BottomNavbar extends State<BottomNavbarState> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    HomeScreen(),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-    Text('Index', style: optionStyle),
-    Text('Index', style: optionStyle),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
+class _BottomNavBar extends State<BottomNavBar>{
+  @override:
+  Widget build(BuildContext context){
     return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.accessibility),
-            label: 'Run',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info),
-            label: 'Info',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.green,
-        onTap: _onItemTapped,
-      ),
+      bottomNavigationBar: GNav(
+      tabs: [
+        GButton(icon: Icons.home),
+        GButton(icon: Icons.people),
+        GButton(icon: Icons.run_circle),
+        GButton(icon: Icons.settings)
+      ]
+      ), 
     );
   }
 }
