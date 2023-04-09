c60eb17894eb19960fe0068e589821f508ee87fc
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2023-04-08 22:22:25 -0700

    fixed bottom navbar

--- lib/Screens/BottomNavbar.dart
+++ lib/Screens/BottomNavbar.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: file_names
-
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:pacepals/Screens/CommunityScreen.dart';
+import 'package:pacepals/Screens/RunScreen.dart';
+import 'package:pacepals/Screens/SettingScreen.dart';
 import 'HomeScreen.dart';
 
 void main() => runApp(const BottomNavBar());
@@ -15,23 +16,17 @@ class BottomNavBar extends StatefulWidget {
 class _BottomNavBar extends State<BottomNavBar> {
   int _currentIndex = 0;
 
-  void _onTabChange(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     Widget currentScreen;
     if (_currentIndex == 0) {
       currentScreen = HomeScreen();
     } else if (_currentIndex == 1) {
-      currentScreen = HomeScreen();
+      currentScreen = const CommunityScreen();
     } else if (_currentIndex == 2) {
-      currentScreen = HomeScreen();
+      currentScreen = const RunScreen();
     } else {
-      currentScreen = HomeScreen();
+      currentScreen = const SettingScreen();
     }
 
     return Scaffold(
@@ -41,21 +36,25 @@ class _BottomNavBar extends State<BottomNavBar> {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
           child: GNav(
-              backgroundColor: Colors.black,
-              color: Colors.green,
-              activeColor: Colors.white,
-              tabBackgroundColor: Colors.green.shade700,
-              gap: 8,
-              onTabChange: (index) {
-                print(index);
-              },
-              padding: EdgeInsets.all(16),
-              tabs: const [
-                GButton(icon: Icons.home, text: "Home"),
-                GButton(icon: Icons.people, text: 'Community'),
-                GButton(icon: Icons.run_circle, text: 'Run'),
-                GButton(icon: Icons.settings, text: 'Settings')
-              ]),
+            backgroundColor: Colors.black,
+            color: Colors.green,
+            activeColor: Colors.white,
+            tabBackgroundColor: Colors.green.shade700,
+            gap: 8,
+            onTabChange: (index) {
+              setState(() {
+                _currentIndex = index;
+                print(_currentIndex);
+              });
+            },
+            padding: EdgeInsets.all(16),
+            tabs: const [
+              GButton(icon: Icons.home, text: "Home"),
+              GButton(icon: Icons.people, text: 'Community'),
+              GButton(icon: Icons.run_circle, text: 'Run'),
+              GButton(icon: Icons.settings, text: 'Settings')
+            ],
+          ),
         ),
       ),
     );
